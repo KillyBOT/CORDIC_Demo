c51e41b810b4eaef0d6de9a9c7d0a1497a1e6a13
KillyBOT <edwards.kyle.j@gmail.com>
2020-02-26 11:01:59 -0500

    I added basic complex value functionality

--- cordic.c
+++ cordic.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cordic.h"
 
 int main(int argc, char* argv[]){
+  struct cnum a = {1,2};
+  struct cnum b = {2,1};
 
+  print_cnum(c_mult(a,b));
+  print_cnum(c_conj(a));
+}
+
+struct cnum c_mult(struct cnum a, struct cnum b){
+  struct cnum c = {0,0};
+  c.r = (a.r*b.r)-(a.i*b.i);
+  c.i = (a.r*b.i)+(a.i+b.r);
+
+  return c;
+}
+
+struct cnum c_conj(struct cnum c){
+  struct cnum cf = {c.r,-c.i};
+  return cf;
+}
+
+void print_cnum(struct cnum c){
+  printf("%f + %fi\n", c.r, c.i);
 }
--- cordic.h
+++ cordic.h
@@ -1,6 +1,14 @@
 #ifndef CORDIC_H
 #define CORDIC_H
 
+struct cnum{
+  double r;
+  double i;
+};
 
+struct cnum c_mult(struct cnum a, struct cnum b);
+struct cnum c_conj(struct cnum c);
+
+void print_cnum(struct cnum c);
 
 #endif
